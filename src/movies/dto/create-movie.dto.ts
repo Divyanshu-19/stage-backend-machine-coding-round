9905903880ae5a32cd42ac01ba85946937d4fb26
Divyanshu Gupta <divyanshu@Divyanshus-Mac-mini.local>
2025-02-01 12:08:03 +0530

    fix: create movie date transform

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -4,36 +4,52 @@ import {
   IsArray,
   IsDate,
   ArrayMinSize,
+  MinLength,
+  MaxLength,
+  IsEnum,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { genre } from 'src/constants/constants';
 export class CreateMovieDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @MinLength(1)
+  @MaxLength(200)
   title: string;
 
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @MinLength(10)
+  @MaxLength(1000)
   description: string;
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({  enum: genre, isArray: true })
   @IsNotEmpty()
   @IsArray()
   @ArrayMinSize(1)
+  @IsEnum(genre, { each: true })
   genres: string[];
 
   @ApiProperty({ type: Date })
   @IsNotEmpty()
   @IsDate()
+  @Transform(({ value }) => new Date(value))
   releaseDate: Date;
 
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   director: string;
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty()
   @IsArray()
+  @IsString({ each: true })
+  @MinLength(2, { each: true })
+  @MaxLength(100, { each: true })
   actors: string[];
 }
